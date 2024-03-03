5e0a7a3b2a0209af82725d1eec21b8fa11df6859
Leegeonwoo7 <workgeon07@naver.com>
2024-03-03 21:09:36 +0900

    basic java code

--- src/main/java/com/spring_parc/App01GamingBasicJava.java
+++ src/main/java/com/spring_parc/App01GamingBasicJava.java
@@ -1,14 +1,11 @@
 package com.spring_parc;
 
-import com.spring_parc.game.GameRunner;
-import com.spring_parc.game.PackManGame;
-
 public class App01GamingBasicJava {
     public static void main(String[] args){
 //        MarioGame game = new MarioGame();
 //        SuperContraGame game = new SuperContraGame();
-        PackManGame game = new PackManGame();
-        var gameRunner = new GameRunner(game);
-        gameRunner.run();
+//        PackManGame game = new PackManGame();
+//        var gameRunner = new GameRunner(game);
+//        gameRunner.run();
     }
 }
